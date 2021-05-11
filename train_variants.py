1e19e20099188dea42c97f09de39a0fadbcba92f
Dani-97 <daniel.iglesias.moris@outlook.es>
2021-05-11 12:00:13 +0200

    Changes made in order to make possible execute the code with tensorflow 2.4.x

--- train_variants.py
+++ train_variants.py
@@ -1,7 +1,10 @@
 import os
 from datetime import datetime
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
 import gc
 
 import train_loop
